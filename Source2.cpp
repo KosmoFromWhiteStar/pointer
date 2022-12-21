6e1900f16f1cbd113eb30a14325acfd032203d82
Kosmo <The_Kosmo@outlook.com>
2022-12-21 20:06:39 +0500

    сд

--- Source2.cpp
+++ Source2.cpp
@@ -3,22 +3,36 @@
 
 bool substr(const char* str, const char* str_2)
 {
-	std::cout << sizeof(str);
-	for (int c = 0; str[c] != '\0' || str_2[c] != '\0'; c++)
+	bool fluger = false;
+	
+	for (int c = 0; str[c] != '\0'; c++)
 	{
-		if (str[c] != str_2[c]) return false;
+		if (str[c] == str_2[0])
+		{
+			fluger = true;
+			for (int p = 1; str[c + p] != '\0' && str_2[p] != '\0'; p++)
+			{
+				if (str[c + p] != str_2[p])
+				{
+					fluger = false;
+					break;
+				}
+			}
+			if (fluger) return fluger;
+		}
 	}
-	return true;
+
+	return fluger;
 }
 
 int main() 
 {
 	const char* a = "Hello world";
 
-	const char* b = "wor";
+	const char* b = "lo";
 
 	const char* c = "banana";
 	
-	substr(a, b);
+	std::cout << (substr(a, b) ? "T" : "F") << " " << (substr(a, c) ? "T" : "F");
 	return 0;
 }
